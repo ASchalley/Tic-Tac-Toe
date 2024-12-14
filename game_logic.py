0fddeb5bdf8202fc346c6ae1fe092c9487d41ac3
ASchalley <aschalley@unomaha.edu>
2024-12-13 21:32:46 -0600

    Out of time

--- game_logic.py
+++ game_logic.py
@@ -5,7 +5,7 @@ from gamestart import *
 from gameboard import *
 from random import *
 
-class GameStart(QMainWindow, Ui_MainWindow1):
+class GameStart(QMainWindow, Ui_MainWindow):
 
     def __init__(self) -> None:
         """
@@ -47,6 +47,8 @@ class GameStart(QMainWindow, Ui_MainWindow1):
             first = random.choice([1, 2])  # https://www.geeksforgeeks.org/random-numbers-in-python/
         file = open("data.txt", "w")
         file.write(str(diff) + "\n")
+        if diff == 4:
+            first = 2
         file.write(str(first))
         file.close()
 
@@ -59,9 +61,11 @@ class GameBoard(QMainWindow, Ui_MainWindow2):
         """
         self.__difficulty = 1
         self.__first = 1
+        self.__chosen = []
         super().__init__()
         self.takesettings()
         self.setupUi(self)
+        self.submitButton.clicked.connect( lambda : self.humanmove())
 
     def takesettings(self) -> None:
         """
@@ -72,3 +76,133 @@ class GameBoard(QMainWindow, Ui_MainWindow2):
         self.__difficulty = content[0]
         self.__first = content[1]
         file.close()
+
+    def computermove(self) -> None:
+        """
+        Method to place computers O
+        """
+        if self.__difficulty == 1:
+            move = random.choice([1,2,3,4,5,6,7,8,9])
+            while move in self.__chosen:
+                move = random.choice([1,2,3,4,5,6,7,8,9])
+        elif self.__difficulty == 2:
+            pass
+        elif self.__difficulty == 3:
+            pass
+        elif self.__difficulty == 4:
+            pass
+
+
+    def humanmove(self) -> None:
+        """
+        Method to submit human move
+        """
+        if self.spot1.isChecked():
+            self.spot1.hide()
+            self.label1.setText("X")
+            self.__chosen.append(1)
+            self.computermove()
+        elif self.spot2.isChecked():
+            self.spot2.hide()
+            self.label2.setText("X")
+            self.__chosen.append(2)
+            self.computermove()
+        elif self.spot3.isChecked():
+            self.spot3.hide()
+            self.label3.setText("X")
+            self.__chosen.append(3)
+            self.computermove()
+        elif self.spot4.isChecked():
+            self.spot4.hide()
+            self.label4.setText("X")
+            self.__chosen.append(4)
+            self.computermove()
+        elif self.spot5.isChecked():
+            self.spot5.hide()
+            self.label5.setText("X")
+            self.__chosen.append(5)
+            self.computermove()
+        elif self.spot6.isChecked():
+            self.spot6.hide()
+            self.label6.setText("X")
+            self.__chosen.append(6)
+            self.computermove()
+        elif self.spot7.isChecked():
+            self.spot7.hide()
+            self.label7.setText("X")
+            self.__chosen.append(7)
+            self.computermove()
+        elif self.spot8.isChecked():
+            self.spot8.hide()
+            self.label8.setText("X")
+            self.__chosen.append(8)
+            self.computermove()
+        elif self.spot9.isChecked():
+            self.spot9.hide()
+            self.label9.setText("X")
+            self.__chosen.append(9)
+            self.computermove()
+
+    def wincheck(self) -> None:
+        """
+        Method to check if a move wins.
+        """
+        if self.label1 == "X":
+            if self.label2 == "X":
+                if self.label3 == "X":
+                    self.placeLabel.setText("You Win!")
+            if self.label4 == "X":
+                if self.label7 == "X":
+                    self.placeLabel.setText("You Win!")
+            if self.label5 == "X":
+                if self.label9 == "X":
+                    self.placeLabel.setText("You Win!")
+        if self.label2 == "X":
+            if self.label5 == "X":
+                if self.label8 == "X":
+                    self.placeLabel.setText("You Win!")
+        if self.label3 == "X":
+            if self.label6 == "X":
+                if self.label9 == "X":
+                    self.placeLabel.setText("You Win!")
+            if self.label5 == "X":
+                if self.label7 == "X":
+                    self.placeLabel.setText("You Win!")
+        if self.label4 == "X":
+            if self.label5 == "X":
+                if self.label6 == "X":
+                    self.placeLabel.setText("You Win!")
+        if self.label7 == "X":
+            if self.label8 == "X":
+                if self.label9 == "X":
+                    self.placeLabel.setText("You Win!")
+
+        if self.label1 == "O":
+            if self.label2 == "O":
+                if self.label3 == "O":
+                    self.placeLabel.setText("You Lose!")
+            if self.label4 == "O":
+                if self.label7 == "O":
+                    self.placeLabel.setText("You Lose!")
+            if self.label5 == "O":
+                if self.label9 == "O":
+                    self.placeLabel.setText("You Lose!")
+        if self.label2 == "O":
+            if self.label5 == "O":
+                if self.label8 == "O":
+                    self.placeLabel.setText("You Lose!")
+        if self.label3 == "O":
+            if self.label6 == "O":
+                if self.label9 == "O":
+                    self.placeLabel.setText("You Lose!")
+            if self.label5 == "O":
+                if self.label7 == "O":
+                    self.placeLabel.setText("You Lose!")
+        if self.label4 == "O":
+            if self.label5 == "O":
+                if self.label6 == "O":
+                    self.placeLabel.setText("You Lose!")
+        if self.label7 == "O":
+            if self.label8 == "O":
+                if self.label9 == "O":
+                    self.placeLabel.setText("You Lose!")
--- gamestart.py
+++ gamestart.py
@@ -9,7 +9,7 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow1(object):
+class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(500, 400)
