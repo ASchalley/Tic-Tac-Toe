7b384cecd5fe81c0a1c56a48c34ef34b5c69657e
ASchalley <aschalley@unomaha.edu>
2024-12-14 12:39:20 -0600

    Working?

--- game_logic.py
+++ game_logic.py
@@ -81,70 +81,95 @@ class GameBoard(QMainWindow, Ui_gameBoard):
         """
         file = open("data.txt", "r")
         content = file.readlines() # https://www.geeksforgeeks.org/how-to-read-specific-lines-from-a-file-in-python/
-        self.__difficulty = content[0]
-        self.__first = content[1]
+        self.__difficulty = int(content[0])
+        self.__first = int(content[1])
         file.close()
 
     def resetboard(self):
         self.__chosen = []
         self.replayButton.hide()
         self.submitButton.show()
+        self.label1.setText("")
+        self.label2.setText("")
+        self.label3.setText("")
+        self.label4.setText("")
+        self.label5.setText("")
+        self.label6.setText("")
+        self.label7.setText("")
+        self.label8.setText("")
+        self.label9.setText("")
+        self.spot1.show()
+        self.spot2.show()
+        self.spot3.show()
+        self.spot4.show()
+        self.spot5.show()
+        self.spot6.show()
+        self.spot7.show()
+        self.spot8.show()
+        self.spot9.show()
 
     def computermove(self) -> None:
         """
         Method to place computers O
         """
-        print("start computer move")
         move = None
+        print(f"difficulty {self.__difficulty}")
         if self.__difficulty == 1:
-            move = 1#random.choice([1,2,3,4,5,6,7,8,9])
-            #while move in self.__chosen:
-                #move = random.choice([1,2,3,4,5,6,7,8,9])
+            move = random.choice([1,2,3,4,5,6,7,8,9])
+            while move in self.__chosen:
+                move = random.choice([1,2,3,4,5,6,7,8,9])
         elif self.__difficulty == 2:
             pass
         elif self.__difficulty == 3:
             pass
         elif self.__difficulty == 4:
             pass
-        print("place computer move")
         if move == 1:
+            print("move placed")
             self.spot1.hide()
             self.label1.setText("O")
             self.__chosen.append(1)
+            self.wincheck(2)
         elif move == 2:
             self.spot2.hide()
             self.label2.setText("O")
             self.__chosen.append(2)
+            self.wincheck(2)
         elif move == 3:
-            self.spot2.hide()
+            self.spot3.hide()
             self.label3.setText("O")
             self.__chosen.append(3)
+            self.wincheck(2)
         elif move == 4:
             self.spot4.hide()
             self.label4.setText("O")
             self.__chosen.append(4)
+            self.wincheck(2)
         elif move == 5:
             self.spot5.hide()
             self.label5.setText("O")
             self.__chosen.append(5)
+            self.wincheck(2)
         elif move == 6:
             self.spot6.hide()
             self.label6.setText("O")
             self.__chosen.append(6)
+            self.wincheck(2)
         elif move == 7:
             self.spot7.hide()
             self.label7.setText("O")
             self.__chosen.append(7)
+            self.wincheck(2)
         elif move == 8:
             self.spot8.hide()
             self.label8.setText("O")
             self.__chosen.append(8)
+            self.wincheck(2)
         elif move == 9:
             self.spot9.hide()
             self.label9.setText("O")
             self.__chosen.append(9)
-        print("end computer move")
-        self.wincheck(2)
+            self.wincheck(2)
 
     def winscreen(self, winner):
         self.submitButton.hide()
@@ -153,6 +178,8 @@ class GameBoard(QMainWindow, Ui_gameBoard):
             self.__humanWins += 1
         elif winner == 2:
             self.__computerWins += 1
+        elif winner == 3:
+            self.placeLabel.setText("It's a Tie!")
         self.humanWinLabel.setText(f"Human Wins: {self.__humanWins}")
         self.computerWinLabel.setText(f"Computer Wins: {self.__computerWins}")
 
@@ -164,47 +191,53 @@ class GameBoard(QMainWindow, Ui_gameBoard):
             self.spot1.hide()
             self.label1.setText("X")
             self.__chosen.append(1)
+            self.wincheck(1)
         elif self.spot2.isChecked():
             self.spot2.hide()
             self.label2.setText("X")
             self.__chosen.append(2)
+            self.wincheck(1)
         elif self.spot3.isChecked():
             self.spot3.hide()
             self.label3.setText("X")
             self.__chosen.append(3)
+            self.wincheck(1)
         elif self.spot4.isChecked():
             self.spot4.hide()
             self.label4.setText("X")
             self.__chosen.append(4)
+            self.wincheck(1)
         elif self.spot5.isChecked():
             self.spot5.hide()
             self.label5.setText("X")
             self.__chosen.append(5)
+            self.wincheck(1)
         elif self.spot6.isChecked():
             self.spot6.hide()
             self.label6.setText("X")
             self.__chosen.append(6)
+            self.wincheck(1)
         elif self.spot7.isChecked():
             self.spot7.hide()
             self.label7.setText("X")
             self.__chosen.append(7)
+            self.wincheck(1)
         elif self.spot8.isChecked():
             self.spot8.hide()
             self.label8.setText("X")
             self.__chosen.append(8)
+            self.wincheck(1)
         elif self.spot9.isChecked():
             self.spot9.hide()
             self.label9.setText("X")
             self.__chosen.append(9)
-        self.wincheck(1)
+            self.wincheck(1)
 
     def wincheck(self, player) -> None:
         """
         Method to check if a move wins.
         """
-        print("wincheck reached")
-        print(player)
-        if player == "1":
+        if player == 1:
             if self.label1.text() == "X":
                 if self.label2.text() == "X":
                     if self.label3.text() == "X":
@@ -234,12 +267,12 @@ class GameBoard(QMainWindow, Ui_gameBoard):
                 if self.label8.text() == "X":
                     if self.label9.text() == "X":
                         self.placeLabel.setText("You Win!")
-        print("test%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-        #if self.placeLabel.text() == "You Win!": # PROBLEM CHILD ****************************************************************************************************************************
-            #self.winscreen(1)
-        #else:
-            #self.computermove()
-        print("computer wincheck time **********")
+            text = self.placeLabel.text()
+            if text == "You Win!":
+                self.winscreen(1)
+            else:
+                self.tiecheck()
+                self.computermove()
         if player == "2":
             if self.label1.text() == "O":
                 if self.label2.text() == "O":
@@ -270,10 +303,14 @@ class GameBoard(QMainWindow, Ui_gameBoard):
                 if self.label8.text() == "O":
                     if self.label9.text() == "O":
                         self.placeLabel.setText("You Lose!")
-        print("wincheck finished")
-        if self.placeLabel.text() == "You Lose!":
-            self.winscreen(2)
-        else:
-            #self.humanmove()
-            print("human move time")
-            exit()
+            text = self.placeLabel.text()
+            if text == "You Lose!":
+                self.winscreen(2)
+            else:
+                self.tiecheck()
+                self.humanmove()
+
+    def tiecheck(self):
+        print (len(self.__chosen))
+        if len(self.__chosen) >= 9:
+            self.winscreen(3)
